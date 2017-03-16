2beafad3b4f409eb4f214ad81899e5a03c06a3de
k2751 <k2751@student.jamk.fi>
2017-03-16 09:10:46 +0200

    ,bghgvl

--- NavigationExample/App.xaml.cs
+++ NavigationExample/App.xaml.cs
@@ -22,6 +22,9 @@ namespace NavigationExample
     /// </summary>
     sealed partial class App : Application
     {
+        //omat tärkeät ominaisuudet ja metodit
+
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
